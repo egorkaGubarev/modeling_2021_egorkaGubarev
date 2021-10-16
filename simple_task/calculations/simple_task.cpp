b65073abe42eb908a9aed43a200fc7f812b8f34e
egorkaGubarev <gubarev.ed@phystech.edu>
2021-10-16 08:53:59 +0300

    Add plots of velocities, energy and energy error

--- simple_task/calculations/simple_task.cpp
+++ simple_task/calculations/simple_task.cpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <iostream>
+#include <math.h>
 #include <string>
 #include <vector>
 
@@ -11,28 +13,52 @@ void clear_file(const std::string path)
     file.close();
 }
 
-std::vector<std::vector<float>> solve_simple_task(const float a_start, const float b_start, const float time_end, const float time_step, const uint k=1)
+std::vector<std::vector<std::vector<float>>> solve_simple_task(const float a_start, const float b_start, const float time_end,
+                                                               const std::vector<float> time_steps,
+                                                               const uint k = 1)
 {
-    std::vector<float> a, b;
-    const uint amount_iterations = time_end / time_step;
-    a.resize(amount_iterations);
-    b.resize(amount_iterations);
-    a[0] = a_start;
-    b[0] = b_start;
-    for (uint iteration = 1; iteration < amount_iterations; ++ iteration){
-        a[iteration] = a[iteration - 1] + b[iteration - 1] * time_step;
-        b[iteration] = b[iteration - 1] - a[iteration - 1] * k * time_step;
+    uint time_step_number = 0;
+    uint time_steps_amount = time_steps.size();
+    std::vector<std::vector<std::vector<float>>> solution(time_steps_amount);
+    for (float time_step: time_steps){
+        std::vector<float> a, b, energy, time;
+        const uint amount_iterations = time_end / time_step;
+        a.resize(amount_iterations);
+        b.resize(amount_iterations);
+        energy.resize(amount_iterations);
+        time.resize(amount_iterations);
+        a[0] = a_start;
+        b[0] = b_start;
+        float kinetic_energy = std::pow(b_start, 2) / 2;
+        float potential_energy = k * std::pow(a_start, 2) / 2;
+        energy[0] = kinetic_energy + potential_energy;
+        time[0] = 0;
+        for (uint iteration = 1; iteration < amount_iterations; ++ iteration){
+            a[iteration] = a[iteration - 1] + b[iteration - 1] * time_step;
+            b[iteration] = b[iteration - 1] - a[iteration - 1] * k * time_step;
+            kinetic_energy = std::pow(b[iteration], 2) / 2;
+            potential_energy = k * std::pow(a[iteration], 2) / 2;
+            energy[iteration] = kinetic_energy + potential_energy;
+            time[iteration] = time_step * iteration;
+        }
+        solution[time_step_number] = {a, b, energy, time};
+        std::cout << "Solution with time step " << time_step << " is founded!" << '\n';
+        ++ time_step_number;
     }
-    std::vector<std::vector<float>> solution = {a, b};
     return solution;
 }
 
-void write_vector_to_file(const std::vector<float> vector_1, const std::string path)
+void write_vector_to_file(const std::vector<float> data, const std::string path, const uint period)
 {
     std::ofstream file;
     file.open(path, std::ios::app);
-    for (float value: vector_1){
-        file << value << ";";
+    uint size = data.size();
+    for (uint index = 0; index < size; ++ index){
+        uint mod = index % period;
+        if (mod == 0){
+            const float value = data[index];
+            file << value << ";";
+        }
     }
     file << '\n';
     file.close();
@@ -40,16 +66,30 @@ void write_vector_to_file(const std::vector<float> vector_1, const std::string p
 
 int main(int argc, char** argv)
 {
+    const uint numbers_to_write = 10;
     const float a_start = 6371000;
     const float b_start = 0;
-    const float time_end = 100;
-    const float time_step = 1;
+    const float time_end = 1;
+    const std::vector<float> time_steps = {0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001};
     const std::string path = "result.txt";
-    const std::vector<std::vector<float>> solution = solve_simple_task(a_start, b_start, time_end, time_step);
-    const std::vector<float> a = solution[0];
-    const std::vector<float> b = solution[1];
+    const std::vector<std::vector<std::vector<float>>> solution = solve_simple_task(a_start, b_start, time_end, time_steps);
+    uint time_steps_amount = time_steps.size();
     clear_file(path);
-    write_vector_to_file(a, path);
-    write_vector_to_file(b, path);
+    for (uint time_step_number = 0; time_step_number < time_steps_amount; ++ time_step_number)
+    {
+        const std::vector<float> a = solution[time_step_number][0];
+        const std::vector<float> b = solution[time_step_number][1];
+        const std::vector<float> energy = solution[time_step_number][2];
+        const std::vector<float> time = solution[time_step_number][3];
+        const std::vector<float> time_step = {time_steps[time_step_number]};
+        uint numbers_amount = time_end / time_steps[time_step_number];
+        uint period = numbers_amount / numbers_to_write;
+        write_vector_to_file(a, path, period);
+        write_vector_to_file(b, path, period);
+        write_vector_to_file(energy, path, period);
+        write_vector_to_file(time, path, period);
+        write_vector_to_file(time_step, path, 1);
+        std::cout << "Data set number " << time_step_number << " is written!" << '\n';
+    }
     return 0;
 }
--- simple_task/visualization/visualization.py
+++ simple_task/visualization/visualization.py
@@ -1,19 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-path: str = "C:\\Programs\\Git\\repositories\\modeling_2021_egorkaGubarev\\simple_task\\calculations\\result.txt"
+data_sets_amount: int = 8
+strings_amount_in_data_set = 5
+path: str = "C:/Programs/Git/repositories/modeling_2021_egorkaGubarev/simple_task/calculations/result.txt"
 symbol_split: str = ";"
 
+figure, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+energy_delta_list: list = []
+time_step_list: list = []
 with open(path, 'r') as file:
-    a_str: str = file.readline()
-    b_str: str = file.readline()
-a = np.array(a_str.split(symbol_split))
-b = np.array(b_str.split(symbol_split))
-a = a[0:a.size - 1]
-b = b[0:b.size - 1]
-a = list(map(float, a))
-b = list(map(float, b))
-plt.plot(a, b)
-plt.xlabel('a')
-plt.ylabel('b')
+    for data_set_number in range(data_sets_amount):
+        data_list: list = []
+        for string_number in range(strings_amount_in_data_set):
+            string: str = file.readline()
+            array = np.array(string.split(symbol_split))
+            data_size: int = array.size - 1
+            array_data = array[0:data_size]
+            numbers: list = list(map(float, array_data))
+            data_list.append(numbers)
+        x: list = data_list[0]
+        v: list = data_list[1]
+        energy: list = data_list[2]
+        time: list = data_list[3]
+        time_step: list = data_list[4]
+        time_step_ln: float = np.log(time_step)
+        energy_start: float = energy[0]
+        energy_end: float = energy[-1]
+        energy_delta: float = energy_end - energy_start
+        energy_delta_ln: float = np.log(energy_delta)
+        energy_delta_list.append(energy_delta_ln)
+        time_step_list.append(time_step_ln)
+        ax1.plot(time, x)
+        ax2.plot(time, v)
+        ax3.plot(x, v)
+        ax4.plot(time, energy)
+ax1.set_title('x(t)')
+ax1.set_xlabel('t')
+ax1.set_ylabel('x')
+ax2.set_title('v(t)')
+ax2.set_xlabel('t')
+ax2.set_ylabel('v')
+ax3.set_title('v(x)')
+ax3.set_xlabel('x')
+ax3.set_ylabel('v')
+ax4.set_label('E(t)')
+ax4.set_xlabel('t')
+ax4.set_ylabel('E')
+ax5.plot(time_step_list, energy_delta_list)
+ax5.set_label('ErrorE(t)')
+ax5.set_xlabel('ln(t)')
+ax5.set_ylabel('ln(ErrorE)')
 plt.show()
